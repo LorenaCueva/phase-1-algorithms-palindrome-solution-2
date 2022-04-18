ee81924962288b69589dc9a0ee924767e25b576b
Lorena Cueva <lorenacueva@gmail.com>
2022-04-18 14:56:38 -0700

    commit tree 6cff4c7f108d76cbd87bf90fc887be6493c6a850

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  wordArr = word.split('')
+  for(let i = 0, y = wordArr.length-1; i <= y; i++, y--){
+    if(wordArr[i]!= wordArr[y]){
+      return false
+    }
+  }
+  return true
 }
 
 /* 
@@ -12,14 +18,38 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome(""));
 }
 
 module.exports = isPalindrome;
